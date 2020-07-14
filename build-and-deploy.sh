b1a49926254c42729bbb1bc694c81faa15a2606c
Kris De Volder <kdevolder@pivotal.io>
2020-07-14 16:32:22 -0700

    Try to simplify some jquery

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,9 +2,9 @@
 set -e
 #docker build -t kdvolder/lab-spring-boot-k8s-getting-started .
 #docker push kdvolder/lab-spring-boot-k8s-getting-started
-kubectl delete -f resources/workshop.yaml
-kubectl apply -f resources/workshop.yaml
-kubectl delete -f resources/training-portal.yaml
-kubectl apply -f resources/training-portal.yaml
 kubectl delete --namespace=lab-spring-boot-k8s-gs-w01 pods --all
+kubectl delete -f resources/workshop.yaml
+kubectl delete -f resources/training-portal.yaml
+kubectl apply -f resources/workshop.yaml
+kubectl apply -f resources/training-portal.yaml
 watch kubectl get eduk8s
